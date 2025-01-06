7e1db87c91b8c49bd25925f955e17ae2905c58db
ninah <malalaninah6@gmail.com>
2025-01-06 09:57:53 +0300

    debut conception perso

--- sql/conception_perso/schema.sql
+++ sql/conception_perso/schema.sql
@@ -91,14 +91,63 @@ CREATE DATABASE boulangerie;
             JOIN details_commande dc ON c.id = dc.id_commande
         GROUP BY c.id;
 
+        create type_statut_commande(
+            livre (finis ), en_cours , en_attente (pas encore fais ), annuler
+        )
 
         create table statut_commande(
             id_statut_commande
             id_commande
+            type_statut_commande
+            id_user
+            date
         )
 
 
-CREATE TABLE unitx(
+
+        ---gestion client 
+            client
+                CREATE TABLE client (
+                    id SERIAL PRIMARY KEY,
+                    nom VARCHAR(255) NOT NULL
+                );
+                client_fidele (vue calculée dynamiquement) == commande >= 10 in 1 mont 
+                CREATE VIEW client_fidele AS
+                SELECT 
+                    c.id AS id_client,
+                    c.nom,
+                    COUNT(cmd.id) AS nb_commandes,
+                    CASE 
+                        WHEN COUNT(cmd.id) > 10 THEN 'FIDÈLE'
+                        ELSE 'NON FIDÈLE'
+                    END AS statut_fidelite
+                FROM client c
+                LEFT JOIN commande cmd ON c.id = cmd.id_client
+                WHERE cmd.date_commande >= (CURRENT_DATE - INTERVAL '1 MONTH')
+                GROUP BY c.id, c.nom;
+        --upadte statut_commande ==> generate facture
+            CREATE TABLE facture (
+                    id SERIAL PRIMARY KEY,
+                    id_tot INT REFERENCES commande(id),
+                    date_facture TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    id_client INT REFERENCES client(id) -- ajouter une référence à client
+                            );
+
+            statut_facture(
+                                idpk
+                                payer or pas
+                                id facture fk
+                                descprtion
+                                justificatif
+                                id_user)
+
+
+
+
+
+--gestion production ++mvt stock, gestion stock ingredient 
+
+CREATE TABLE unit(
     id_unit SERIAL PRIMARY KEY, 
     nom_unit VARCHAR(255) NOT NULL
 );
@@ -106,19 +155,49 @@ CREATE TABLE unitx(
 CREATE TABLE ingredient (
     id_ingredient SERIAL PRIMARY KEY,
     ingredient_name VARCHAR(255) NOT NULL,
-    unit INT,
-    price INT NOT NULL DEFAULT 0 
+    id_unit INT,
+    price INT NOT NULL DEFAULT 0,
+
 );
 
+create table stock_ingredient (
+        id_stock_ingredient
+        id_ingredient
+        qtt,
+        date_last_update
+        id_user
+)
+
+CREATE TABLE mvt_stock_ingredient (
+    id_mvt_stock_ingredient SERIAL PRIMARY KEY,
+    id_stock_ingredient
+    quantite DECIMAL(10, 2) NOT NULL,
+    statut VARCHAR(10) CHECK (statut IN ('IN', 'OUT')) NOT NULL,
+    date_mouvement TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id_user
+);
+
+
 CREATE TABLE recipe_ingredient (
+    id_recipe_ingredient
     id_recipe INT,
     id_ingredient INT,
     quantity DECIMAL(10,2), -- To store the amount needed for each recipe
-    PRIMARY KEY (id_recipe, id_ingredient),
+    PRIMARY KEY id_recipe_ingredient),
     FOREIGN KEY (id_recipe) REFERENCES recipe(id_recipe),
     FOREIGN KEY (id_ingredient) REFERENCES ingredient(id_ingredient)
 );
 
+
+--stock ingredient ++ 
+create table production(
+    id_production
+    id_recipe
+    qtt
+    date 
+    id_user 
+
+)
 CREATE TABLE step (
     id_step SERIAL PRIMARY KEY,
     id_recipe INT NOT NULL,
