1e10c76535b25b5ae47bb274365d9f87dbe9c8aa
ninah <malalaninah6@gmail.com>
2025-01-21 15:23:18 +0300

    donne update

--- sql/alea5/data.sql
+++ sql/alea5/data.sql
@@ -1,12 +1,9 @@
+-- Insertion d'une commission de 5% pour chaque utilisateur depuis le 1er janvier 2024 jusqu'à NULL
 INSERT INTO commission (valuee, date_debut, date_fin, id_vendeur)
-SELECT 
-    5.00 AS valuee,  -- Pourcentage de la commission (5%)
-    '2025-01-01'::TIMESTAMP AS date_debut,  -- Date de début de validité
-    '2025-01-31'::TIMESTAMP AS date_fin,    -- Date de fin de validité
-    v.id_user AS id_vendeur
-FROM 
-    boulangerie_user v
-JOIN 
-    vente ve ON ve.id_user = v.id_user
-WHERE 
-    ve.vente_date BETWEEN '2025-01-01' AND '2025-01-31';
+SELECT
+    5.00 AS valuee,  -- Commission de 5%
+    '2024-01-01'::timestamp AS date_debut,  -- Date de début fixée au 1er janvier 2024
+    '2030-01-01' AS date_fin,  -- Date de fin non définie (NULL)
+    id_user AS id_vendeur
+FROM
+    boulangerie_user;
